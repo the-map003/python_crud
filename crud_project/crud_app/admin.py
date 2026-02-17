34b2e0ad5b36176cc521a070ce2a86dcdfa529f8
the-map003 <aimepatrick2003@gmail.com>
2026-02-17 11:25:37 +0200

    Commit

--- crud_project/crud_app/admin.py
+++ crud_project/crud_app/admin.py
@@ -1,19 +1,3 @@
 from django.contrib import admin
-from .models import Student
 
-
-@admin.register(Student)
-class StudentAdmin(admin.ModelAdmin):
-    list_display = ('roll_number', 'first_name', 'last_name', 'email', 'phone', 'enrollment_date')
-    list_filter = ('enrollment_date', 'date_of_birth')
-    search_fields = ('first_name', 'last_name', 'email', 'roll_number')
-    readonly_fields = ('enrollment_date',)
-    
-    fieldsets = (
-        ('Personal Information', {
-            'fields': ('first_name', 'last_name', 'email', 'phone', 'date_of_birth', 'address')
-        }),
-        ('Academic Information', {
-            'fields': ('roll_number', 'enrollment_date')
-        }),
-    )
+# Register your models here.
--- crud_project/crud_project/settings.py
+++ crud_project/crud_project/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'crud_app',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +54,7 @@ ROOT_URLCONF = 'crud_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
